a80b501f8b7f513a3b99cd5ddb0a175e29e83f21
Harish Kumar <harish@graspio.com>
2020-05-27 14:01:15 +0530

    feat(Purelabel component): create purelabel component with props as specified

--- src/app/components/common/purelabel/index.tsx
+++ src/app/components/common/purelabel/index.tsx
@@ -1,7 +1,53 @@
-import * as React from 'react';
-
+import * as React from "react";
+import style from "./style.css";
+import classNames from "classnames";
 interface PureLabelProps {
-  centreElement: Element;
+    centreElement: React.ReactChild;
+    leftElement?: React.ReactChild;
+    rightElement?: React.ReactChild;
+    backgroundColor?: string | string[];
+    borderRadius?: number;
+    compStyle?: React.CSSProperties;
+    onClick?: (ev: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
-export const PureLabel: React.FC<PureLabelProps> = ({ centreElement }) => <div></div>;
+export const PureLabel: React.FC<PureLabelProps> = ({
+    centreElement,
+    leftElement,
+    rightElement,
+    backgroundColor,
+    borderRadius,
+    compStyle,
+    onClick,
+}) => {
+    const styleLeft = classNames(style.item, style.left);
+    const styleCenter = classNames(style.item, style.center);
+    const styleRight = classNames(style.item, style.right);
+    const styleContainer = classNames(
+        style.container,
+        onClick ? style.btn : null
+    );
+
+    return (
+        <div
+            className={styleContainer}
+            style={{
+                ...(backgroundColor &&
+                    typeof backgroundColor === "string" && {
+                        backgroundColor: backgroundColor,
+                    }),
+                ...(backgroundColor &&
+                    Array.isArray(backgroundColor) && {
+                        background: `linear-gradient(to bottom right, ${backgroundColor[0]}, ${backgroundColor[1]})`,
+                    }),
+                ...(borderRadius && { borderRadius }),
+                ...compStyle,
+            }}
+            onClick={onClick}
+        >
+            <div className={styleLeft}>{leftElement}</div>
+            <div className={styleCenter}>{centreElement}</div>
+            <div className={styleRight}>{rightElement}</div>
+        </div>
+    );
+};
--- src/app/containers/App/index.tsx
+++ src/app/containers/App/index.tsx
@@ -1,11 +1,64 @@
-import React from 'react';
-import style from './style.css';
-import { RouteComponentProps } from 'react-router';
-
+import React from "react";
+import style from "./style.css";
+import { RouteComponentProps } from "react-router";
+import { PureLabel } from "../../components";
 export namespace App {
-  export interface Props extends RouteComponentProps<void> {}
+    export interface Props extends RouteComponentProps<void> {}
 }
 
 export const App = ({ history, location }: App.Props) => {
-  return <div className={style.normal}></div>;
+    const centerElem = <div style={{ color: "white" }}>center</div>;
+    const leftElem = <div style={{ color: "white" }}>left</div>;
+    const rightElem = <div style={{ color: "white" }}>right</div>;
+
+    return (
+        <div className={style.normal}>
+            <h4>Examples</h4>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <h3>with background </h3>
+                    <PureLabel
+                        centreElement={centerElem}
+                        backgroundColor={"#6e84ef"}
+                    />
+                </div>
+
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <h3>with background and radius </h3>
+                    <PureLabel
+                        centreElement={centerElem}
+                        backgroundColor={"#6e84ef"}
+                        borderRadius={15}
+                    />
+                </div>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <h3>with background, borderRadius, left and right elem </h3>
+                    <PureLabel
+                        leftElement={leftElem}
+                        centreElement={centerElem}
+                        rightElement={rightElem}
+                        backgroundColor={"#6e84ef"}
+                        borderRadius={15}
+                    />
+                </div>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <h3>with background gradient and radius </h3>
+                    <PureLabel
+                        centreElement={centerElem}
+                        backgroundColor={["#f85032", "#f16f5c"]}
+                        borderRadius={20}
+                    />
+                </div>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <h3>with background gradient,radius and click </h3>
+                    <PureLabel
+                        centreElement={centerElem}
+                        backgroundColor={["#f85032", "#f16f5c"]}
+                        borderRadius={20}
+                        onClick={() => alert("clicked")}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
